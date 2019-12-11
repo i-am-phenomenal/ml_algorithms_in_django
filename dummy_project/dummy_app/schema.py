c53c71c4df5728df245ff7da18caf42d99bbcd69
Aditya Chaturvedi <39881830+i-am-phenomenal@users.noreply.github.com>
2019-12-11 18:13:35 +0530

    Changes in schema.py

--- dummy_project/dummy_app/schema.py
+++ dummy_project/dummy_app/schema.py
@@ -7,6 +7,12 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class KNPrediction(forms.Form):
+    x_coord = forms.CharField(label="x_coord", max_length=10, required=True)
+    y_coord = forms.CharField(label="y_coord", max_length=10, required=True)
+    color = forms.CharField(label="color", max_length=10, required=True)
+
+
 class KNValuesForm(forms.Form):
     kn_value1 = forms.CharField(
         label="kn_value1", max_length=10, required=True)
@@ -16,6 +22,13 @@ class KNValuesForm(forms.Form):
         label="kn_value3", max_length=10, required=True)
     kn_value4 = forms.CharField(
         label="kn_value4", max_length=10, required=True)
+
+
+class KnPredictionSchema(Base):
+    __tablename__ = 'kn_predictions_algorithm'
+    id = Column(Integer, primary_key=True)
+    x_coord = Column(Integer, nullable=False)
+    y_coord = Column(Integer, nullable=False)
 
 
 class KNClassification(Base):
@@ -28,3 +41,5 @@ class KNClassification(Base):
 
 kn_engine = create_engine('sqlite:///KnClassificationDatabase.db')
 Base.metadata.create_all(kn_engine)
+kn_prediction_engine = create_engine('sqlite:///KnPredictionDatabase.db')
+Base.metadata.create_all(kn_prediction_engine)
