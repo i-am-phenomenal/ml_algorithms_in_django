8dc06e9f2787df796f13c2f2a170977c0085191b
Aditya Chaturvedi <39881830+i-am-phenomenal@users.noreply.github.com>
2019-12-11 18:15:27 +0530

    changes in urls.py

--- dummy_project/dummy_project/urls.py
+++ dummy_project/dummy_project/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from dummy_app.views import home, algo1, decision_tree
+# from django.conf.urls import patterns, url
+from dummy_app.views import home, algo1, decision_tree, kn_algo, custom_kn_algorithm
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('kn_classification/', algo1),
-    path('decision_tree/', decision_tree)
+    path('kn_classification/', kn_algo),
+    path('decision_tree/', decision_tree),
+    path('custom_kn_algorithm/', custom_kn_algorithm),
+    path('$/kn_analysis_of_coordinates/', kn_algo),
+    # path('custom_kn_algorithm/kn_analysis_of_coordinates/', kn_algo)
 ]
