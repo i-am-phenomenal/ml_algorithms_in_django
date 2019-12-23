04668bd9eb0cf30e67c17d92e6fc30a6bd8f4160
Aditya Chaturvedi <39881830+i-am-phenomenal@users.noreply.github.com>
2019-12-23 20:07:01 +0530

    Update kn.py

--- dummy_project/dummy_app/kn.py
+++ dummy_project/dummy_app/kn.py
@@ -1,4 +1,3 @@
-# This file acts as a utility file
 import random
 import math
 import json
@@ -6,29 +5,44 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select
 from sqlalchemy.ext.declarative import declarative_base
+import psycopg2
 
 # PRIVATE FUNSTIONS TO GET ELEMENTS FROM DATASET
 
 
-# def get_mass(element):
-#     if 'mass' in element:
-#         return element['mass']
-#     else:
-#         return 0
+def check_if_contains_decimal(mass_value):
+    if (mass_value.find(".") == -1):
+        return False
+    else:
+        return True
 
 
-# def get_reclat(element):
-#     if 'reclat' in element:
-#         return element['reclat']
-#     else:
-#         return 0.00
+def get_integer_value(mass_value):
+    return mass_value.split('.')[0]
 
 
-# def get_reclong(element):
-#     if 'reclong' in element:
-#         return element['reclong']
-#     else:
-#         return 0.00
+def get_mass(element):
+    if 'mass' in element:
+        if check_if_contains_decimal(element['mass']):
+            return int(get_integer_value(element['mass']))
+        else:
+            return int(element['mass'])
+    else:
+        return 0
+
+
+def get_reclat(element):
+    if 'reclat' in element:
+        return float(element['reclat'])
+    else:
+        return 0.00
+
+
+def get_reclong(element):
+    if 'reclong' in element:
+        return float(element['reclong'])
+    else:
+        return 0.00
 
 
 Base = declarative_base()
@@ -127,3 +141,31 @@ def find_nearest_point(current_object):
 #         session.commit()
 #     except IOError:
 #         print("UNABLE TO SAVE DATA ")
+
+# ------------------ CONNECTIING TO PSQL TABLE AND LOADING SEED DATA -----------------
+
+# connection = psycopg2.connect(database="django_database", user="postgres",
+#                               password="postgres", host="127.0.0.1", port="5432")
+# cursor = connection.cursor()
+# json_file = open('json_dataset.txt', encoding='utf-8-sig')
+# print(json_file)
+# json_data = json.load(json_file)
+# element = json_data[0]
+# print(element)
+# for element in json_data:
+#     # print(element)
+#     command = "INSERT INTO NASA_DATASET (NAME, ID, NAMETYPE, RECCLASS, MASS, FALL, RECLAT, RECLONG) VALUES ('%s', '%d', '%s', '%s', '%d', '%s', '%f', '%f')" % (
+#         element['name'],
+#         int(element['id']),
+#         element['nametype'],
+#         element['recclass'],
+#         get_mass(element),
+#         element['fall'],
+#         get_reclat(element),
+#         get_reclong(element))
+#     # element['reclat'],
+#     # element['reclong'])
+#     cursor.execute(command)
+# connection.commit()
+# connection.close()
+# for record in json_data:
