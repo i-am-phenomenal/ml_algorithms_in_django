d0c82d2375b555d08d383d3579f241c51e355831
Aditya Chaturvedi <39881830+i-am-phenomenal@users.noreply.github.com>
2019-12-23 17:55:26 +0530

    Update settings.py

--- dummy_project/dummy_project/settings.py
+++ dummy_project/dummy_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'dummy_app.apps.DummyAppConfig',
 ]
 
 MIDDLEWARE = [
